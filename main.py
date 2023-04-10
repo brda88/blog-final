ce6de8d113ecf8fc80f1e928ad2bee50c6ba75ef
brda88 <plaksievai@gmail.com>
2023-04-10 17:07:30 +0300

    Add env.

--- main.py
+++ main.py
@@ -9,9 +9,12 @@ from sqlalchemy.orm import relationship
 from flask_login import UserMixin, login_user, LoginManager, login_required, current_user, logout_user
 from forms import CreatePostForm, RegisterForm, LoginForm, CommentForm
 from flask_gravatar import Gravatar
+import os
+from dotenv import load_dotenv
 
+load_dotenv()
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+app.config['SECRET_KEY'] = os.environ.get('MY_KEY')
 ckeditor = CKEditor(app)
 Bootstrap(app)
 
